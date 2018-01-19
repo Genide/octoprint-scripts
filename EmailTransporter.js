af3f135b8621352c79216dc1e844fc8bd87590e5
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2018-01-18 19:46:58 -0600

    Remove debugging code

--- EmailTransporter.js
+++ EmailTransporter.js
@@ -5,13 +5,12 @@ let emailConfig = require('./EmailConfig.json');
 let transporter = nodemailer.createTransport(`smtps://${emailConfig.username}:${emailConfig.password}@${emailConfig.smtp}`);
 
 // Debugging purposes only
-transporter.verify(function(error, success) {
-    if (error) {
-         console.log(error);
-    } else {
-         console.log('Server is ready to take our messages');
-    }
- });
-console.log(process.argv);
+// transporter.verify(function(error, success) {
+//     if (error) {
+//          console.log(error);
+//     } else {
+//          console.log('Server is ready to take our messages');
+//     }
+//  });
 
 module.exports = transporter;
