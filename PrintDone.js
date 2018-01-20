fbcd0cf66dbdb1392549529da0cb6964c39ffbf6
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2018-01-20 14:47:36 -0600

    Update PrintDone script

--- PrintDone.js
+++ PrintDone.js
@@ -3,12 +3,42 @@ let transporter = require('./EmailTransporter');
 let filename = process.argv[2] || 'print';
 let filepath = process.argv[3] || 'unknown';
 let origin = process.argv[4] || 'unknown';  // Can either be local or sdcard
-let time = process.argv[5] || (new Date().toLocaleString('en-US'));
+let timeElapsed = getTimeElapsed(process.argv[5]) || 'unknown';
+let time = new Date().toLocaleString('en-US');
+
+var getTimeElapsed = function (totalSeconds) {
+    secondsRem = parseFloat(totalSeconds);
+    var days = Math.floor(totalSeconds / 3600 / 24);
+    secondsRem %= (3600 * 24);
+
+    var hours = Math.floor(secondsRem / 3600);
+    secondsRem %= 3600;
+
+    var minutes = Math.floor(secondsRem / 60);
+    secondsRem %= 60;
+
+    var seconds = Math.floor(secondsRem / 1);
+
+    var millisec = (secondsRem % 1 * 1000).toPrecision(3);
+
+    var timeElapsed = '';
+    timeElapsed += (days || timeElapsed) ? maybePluralize(days, 'day') + ' ' : '';
+    timeElapsed += (hours || timeElapsed) ? maybePluralize(hours, 'hour') + ' ' : '';
+    timeElapsed += (minutes || timeElapsed) ? maybePluralize(minutes, 'minute') + ' ' : '';
+    timeElapsed += (seconds || timeElapsed) ? maybePluralize(seconds, 'second') + ' ' : '';
+    timeElapsed += (millisec || timeElapsed) ? maybePluralize(millisec, 'millisecond') + ' ' : '';
+
+    return timeElapsed.trim();
+};
+
+var maybePluralize = function (count, noun, suffix = 's') {
+    return `${count} ${noun}${count !== 1 ? suffix : ''}`;  
+};
 
 var message = {
     to: 'danielhuannguyen@gmail.com',
     subject: `Print of ${filename} completed`,
-    text: `Print of ${filename} completed at ${time} from ${origin} storage`,
+    text: `Print of ${filename} completed at ${time} from ${origin} storage. Time elapsed: ${timeElapsed}`,
     attachments:[
         {   // use URL as an attachment
             filename: `${filename}.jpg`,
@@ -16,5 +46,7 @@ var message = {
         }
     ]
 };
+
+
 
 transporter.sendMail(message);
